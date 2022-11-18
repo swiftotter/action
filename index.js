0e963c9f46f4451cdafc967eb61314f51166f226
Navarr <navarr@swiftotter.com>
2022-11-18 15:46:56 -0500

    Only run ssh-agent if there is a private key

--- index.js
+++ index.js
@@ -18,14 +18,13 @@ async function ssh() {
     fs.mkdirSync(sshHomeDir)
   }
 
-  let authSock = '/tmp/ssh-auth.sock'
-  execa.sync('ssh-agent', ['-a', authSock])
-  core.exportVariable('SSH_AUTH_SOCK', authSock)
-
-  let privateKey = core.getInput('private-key')
+  let privateKey = core.getInput('private-key').replace('/\r/g', '').trim();
   if (privateKey !== '') {
-    privateKey = privateKey.replace('/\r/g', '').trim() + '\n'
-    execa.sync('ssh-add', ['-'], {input: privateKey})
+    let authSock = '/tmp/ssh-auth.sock'
+    execa.sync('ssh-agent', ['-a', authSock])
+    core.exportVariable('SSH_AUTH_SOCK', authSock)
+
+    execa.sync('ssh-add', ['-'], {input: privateKey + '\n'})
   }
 
   const knownHosts = core.getInput('known-hosts')
